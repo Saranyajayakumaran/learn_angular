a8f09ddcce18c6f817757ca4ddd40215cc23dccb
Saranya <jsaran.bj@gmail.com>
2025-02-13 09:19:55 +0100

    Learn nesting components

--- first-angular-project/src/app/app.component.ts
+++ first-angular-project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from './navbar/navbar.component';
+import { AppHeader } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AppNavbar,AppHeader],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
