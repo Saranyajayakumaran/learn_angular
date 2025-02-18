cfd3d5d54209b19cad7db5247951596475114a9a
Saranya <jsaran.bj@gmail.com>
2025-02-18 09:53:23 +0100

    Learn External Css file and url

--- first-angular-project/src/app/navbar/navbar.component.ts
+++ first-angular-project/src/app/navbar/navbar.component.ts
@@ -6,11 +6,8 @@ import { Component } from "@angular/core";
     template: `<h1>Navbar Component</h1> 
                 <p> Dummy paragraph </p>
                 <button> Submit </button>`,
-    styles: [`h1{
-                color:red;
-                background:black;
-                }
-            `]
+    templateUrl:'./navbar.component.html',
+    styleUrl:'./navbar.component.css'
 })
 
 export class AppNavbar{
