7b417a2dcec0c008e16a60616d067f1fb8899b01
Saranya <jsaran.bj@gmail.com>
2025-02-18 10:28:18 +0100

    String interpretation

--- first-angular-project/src/app/app.component.ts
+++ first-angular-project/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
-import { AppHeader } from './header/header.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AppNavbar,AppHeader],
+  imports: [RouterOutlet,AppNavbar,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  title: string = 'This loaded dynamically';
 }
--- first-angular-project/src/app/header/header.component.ts
+++ first-angular-project/src/app/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-header',
-    standalone: true,
-    template: '<h1>Header Component</h1>'
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
 })
-
-export class AppHeader{
+export class HeaderComponent {
 
 }
