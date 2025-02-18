f19a230f05b11cecf20f79e4f7e8c50e74a5ec9a
Saranya <jsaran.bj@gmail.com>
2025-02-18 09:36:26 +0100

    Write multiple lines of code

--- first-angular-project/src/app/navbar/navbar.component.ts
+++ first-angular-project/src/app/navbar/navbar.component.ts
@@ -4,8 +4,13 @@ import { Component } from "@angular/core";
     selector: 'app-navbar',
     standalone: true,
     template: `<h1>Navbar Component</h1> 
-    <p> Dummy paragraph </p>
-    <button> Submit </button>`
+                <p> Dummy paragraph </p>
+                <button> Submit </button>`,
+    styles: [`h1{
+                color:red;
+                background:black;
+                }
+            `]
 })
 
 export class AppNavbar{
